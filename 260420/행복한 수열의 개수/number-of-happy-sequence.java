b2c1d7c12d5d982d011702505dbc40a0f7afcb9a
Kim Sanghae <pelikan@pusan.ac.kr>
2026-04-20 15:33:28 +0900

    Title: 행복한 수열의 개수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260420/행복한 수열의 개수/number-of-happy-sequence.java
+++ 260420/행복한 수열의 개수/number-of-happy-sequence.java
@@ -1,69 +1,90 @@
-import java.util.*;
-import java.io.*;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int N, M, board[][];
-    static StringTokenizer st;
-    // static int[] rows, cols;
-    public static void main(String[] args) throws IOException {
-        // Please write your code here.
-        StringBuilder sb = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-       
-        board = new int[N][N];
-        Map<Integer, Integer>[] rows, cols;
-        rows = new Map[N];
-        cols = new Map[N];
-        for(int i = 0 ; i < N ; i++){
-            rows[i] = new HashMap<>();
-            cols[i] = new HashMap<>();
-        }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N, M, board[][];
+	static StringTokenizer st;
 
-        for(int y = 0 ; y < N ; y++ ){
-            st = new StringTokenizer(br.readLine());
-            for(int x = 0 ; x < N ; x++){
-                board[y][x] = Integer.parseInt(st.nextToken());
-                rows[y].merge(board[y][x], 1, Integer::sum);
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        int answer = 0;
-        if(M == 1){
-           answer = (N * 2);
+		board = new int[N][N];
+		Map<Integer, Integer>[] rows, cols;
+		rows = new Map[N];
+		cols = new Map[N];
+		for (int i = 0; i < N; i++) {
+			rows[i] = new HashMap<>();
+			cols[i] = new HashMap<>();
+		}
 
-        }else{
-                    for(int x = 0 ; x < N ; x++ ){
-            for(int y = 0 ; y < N ; y++){
-                cols[y].merge(board[y][x], 1, Integer::sum);
-            }
-        }
+		for (int y = 0; y < N; y++) {
+			st = new StringTokenizer(br.readLine());
+			for (int x = 0; x < N; x++) {
+				board[y][x] = Integer.parseInt(st.nextToken());
+				rows[y].merge(board[y][x], 1, Integer::sum);
+			}
+		}
 
+		int answer = 0;
+		if (M == 1) {
+			answer = (N * 2);
+		} else {
+			for (int x = 0; x < N; x++) {
+				for (int y = 0; y < N; y++) {
+					cols[x].merge(board[y][x], 1, Integer::sum);
+				}
+			}
 
-        for(int y = 0 ; y < N;  y++ ){
-            for(int num = 1 ; num <= 100 ; num++ ){
-                if(rows[y].getOrDefault(num,0) >= M) {
-                    answer++;
-                    continue;
+			for (int y = 0; y < N; y++) {
+                boolean isHappy = false;
+                for(int x = 1 ; x <= N - M + 1 ; x++){
+                    int i = 0;
+                    for(; i < M-1 ; i++){
+                        if(board[y][x+i-1] != board[y][x+i]){
+                            // isHappy = false;
+                            break;
+                        }
+                    }
+                    if(i == M-1) {
+                        answer++;
+                        break;
+                    }    
                 }
-            }
-            for(int num = 1 ; num <= 100 ; num++ ){
-                if(cols[y].getOrDefault(num,0) >= M) {
-                    answer++;
-                    continue;
-                }
-            }
-        }
 
-   
-        }
-     bw.write(answer+"");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+                isHappy = false;
+                for(int x = 1 ; x <= N - M + 1 ; x++){
+                    int i = 0;
+                    for(; i < M -1; i++){
+                        if(board[x+i-1][y] != board[x+i][y]){
+                            // isHappy = false;
+                            break;
+                        }
+                    }
+                    if(i == M-1) {
+                        answer++;
+                      break;
+                    }    
+                }
+
+            }
+		}
+		bw.write(answer + "");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
