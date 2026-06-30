6403c160d59ca1a10ace2d4340239095c93f6a3f
Kim Sanghae <pelikan@pusan.ac.kr>
2026-06-30 19:43:48 +0900

    [Passed] Date to Date (100ms, 9MB)

--- 260630/Date to Date/date-to-date.java
+++ 260630/Date to Date/date-to-date.java
@@ -3,31 +3,27 @@ import java.util.*;
 
 public class Main {
     static int [] days = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
     public static void main(String[] args) throws IOException {
         // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int [][] dates = new int[2][2];
-
-        for(int i = 0 ; i < 2 ; i++){
-            for(int j = 0 ; j < 2 ; j++){
-                dates[i][j] = Integer.parseInt(st.nextToken());
-            }
+        int m1 = Integer.parseInt(st.nextToken());
+        int d1 = Integer.parseInt(st.nextToken());
+        int m2 = Integer.parseInt(st.nextToken());
+        int d2 = Integer.parseInt(st.nextToken());
+        
+   
+        for(int i = 2 ; i <= 12 ; i++){
+            days[i] += days[i-1];
         }
 
-        int firstDates = 0;
-        for(int firstMonth = 1 ; firstMonth < dates[0][0] ; firstMonth++){
-            firstDates += days[firstMonth];
-        }firstDates += (dates[0][1]-1);
+        int firstDates = days[m1-1] + d1;
+        int secondDates = days[m2-1] + d2;
 
-        int secondDates = 0;
-        for(int m = 1 ; m < dates[1][0] ; m++){
-            secondDates += days[m];
-        }secondDates += dates[1][1];
-
-        bw.write((secondDates - firstDates) +"");
+        bw.write((secondDates - firstDates + 1) +"");
         br.close();
         bw.flush();
         bw.close();
